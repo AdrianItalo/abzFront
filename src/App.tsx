81b712f6b37c9a0d0d3640c90534532f391b8eec
AdrianItalo <adrianlopezitalo>
2022-05-02 18:31:05 -0400

    final

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import JqxGrid, { jqx } from "jqwidgets-scripts/jqwidgets-react-tsx/jqxgrid";
 import React, { useState } from "react";
 import { generateData } from "./generatedata";
 
+import JqxTabs from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxtabs';
+
+
 
 import './App.css';
 const MyGrid = () => {
 
     const myGrid = React.createRef<JqxGrid>();
+    
 
     const source = {
         datafields: [
@@ -24,81 +28,122 @@ const MyGrid = () => {
             { name: 'name', type: 'string' },
             { name: 'productname', type: 'string' },
         ],
-        url:'http://127.0.0.1:3333/leer',
-        datatype:'json'
-    }      
-    
+        url: 'http://127.0.0.1:3333/leer',
+        datatype: 'json'
+    }
+    const source3 = {
+        datafields: [
+            { name: 'productname', type: 'string' },
+        ],
+        datatype: 'array',
+        localdata : [
+            {productname: 'Black Tea'},{productname: 'Green Tea'},{productname: 'Caffe Espresso'},{productname: 'Doubleshot Espresso'},{productname: 'Caffe Latte'},
+            {productname: 'White Chocolate Mocha'},{productname: 'Caramel Latte'},{productname: 'Caffe Americano'},{productname: 'Cappuccino'},
+            {productname: 'Espresso Truffle'},{productname: 'Espresso con Panna'},{productname: 'Peppermint Mocha Twist'}
+            
+        ]
+
+    }
+
     const adapter = new jqx.dataAdapter(source);
     const adapter2 = new jqx.dataAdapter(source2);
-    
+    const adapter3 = new jqx.dataAdapter(source3);
+
+       
+
     const exportar = () => {
-        myGrid.current!.exportdata('xls','archivo')
+        myGrid.current!.exportdata('xls', 'archivo')
     }
-        
-    const subir = ( ) => {
+
+    const subir = () => {
 
         setAdap(adapter2)
     }
 
-    const [adap,setAdap] = useState(adapter)
+    const [adap, setAdap] = useState(adapter)
 
-    const cargar =  async (event) => {
+    const cargar = async (event) => {
         const newData = new FormData();
         const data = event.target.files[0]
-        newData.append("excel",data)
+        newData.append("excel", data)
         //console.log(data)
-        const resp = await fetch('http://127.0.0.1:3333/upload',{
+        const resp = await fetch('http://127.0.0.1:3333/upload', {
             method: "POST",
             body: newData
         }).then(resp => resp.json())
         //console.log(resp)
         //alert(JSON.stringify(resp))
 
-        if(resp){
+        if (resp) {
             alert("subido")
             setAdap(adapter2)
         }
     }
 
 
-    return(
+    return (
         <div className="center">
-            <JqxGrid ref={myGrid}
 
-                sortable={true}  
-                filterable={true}
-                pageable={true}
-                showfilterrow={true}
-                source={adap}
-                selectionmode={'multiplecellsadvanced'}
-                editable={true}
-                columns={[{
-                    text: "name",
-                    datafield: "name",
-                    width: "290",
-                    
-                },
-                {
-                    text: "productname",
-                    datafield: "productname",
-                    width: "290",
-                    columntype: 'dropdownlist'
-                }]}
-                
+            <JqxTabs width={610}>
 
-            >
-            </JqxGrid>
-            <div className="centerDiv">
+                <ul>
+                    <li style={{ marginLeft: '30px' }}>Grid 1</li>
+                    <li>Grid 2</li>
+                </ul>
 
-            <JqxButton style={{ marginTop: '5px' }} onClick={exportar}>Export to Excel</JqxButton>
+                <div>
+                    <JqxGrid ref={myGrid}
 
-            <JqxButton style={{ marginTop: '5px' }} onClick={subir}>Subir</JqxButton>
+                        sortable={true}
+                        filterable={true}
+                        pageable={true}
+                        showfilterrow={true}
+                        source={adap}
+                        selectionmode={'multiplecellsadvanced'}
+                        editable={true}
+                        columns={[{
+                            text: "name",
+                            datafield: "name",
+                            width: "290",
 
-                <JqxButton style={{ marginTop: '5px' }}>
-            <input onChange={cargar} name="excel" type='file'/>
-            </JqxButton>
+                        },
+                        {
+                            text: "productname",
+                            datafield: "productname",
+                            width: "290",
+                            columntype: 'dropdownlist'
+                        }]}
 
-            </div>
+
+                    >
+                    </JqxGrid>
+                </div>
+                <div>
+                    <JqxGrid
+                        source={adapter3}
+                        columns={[{
+                            text: "productname",
+                            datafield: "productname",
+                            width: "290",
+                            columntype: 'dropdownlist'
+                        }]}
+                    >
+
+                    </JqxGrid>
+                </div>
+                </JqxTabs>
+                <div className="centerDiv">
+
+                    <JqxButton style={{ marginTop: '5px' }} onClick={exportar}>Export to Excel</JqxButton>
+
+                    <JqxButton style={{ marginTop: '5px' }} onClick={subir}>Actualizar</JqxButton>
+
+                    <JqxButton style={{ marginTop: '5px' }}>
+                        <input onChange={cargar} name="excel" type='file' />
+                    </JqxButton>
+
+                </div>
+            
         </div>
     )
 }
